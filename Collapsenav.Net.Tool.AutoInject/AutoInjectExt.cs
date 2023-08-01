d55c93e29de5cfe64ee8ff531b13337e33421da4
CollapseNav <CollapseNav@163.com>
2023-08-02 00:34:54 +0800

    feat: 自动注入被标记的Type的同时, 尝试注入改元素所在的type

--- Collapsenav.Net.Tool.AutoInject/AutoInjectExt.cs
+++ Collapsenav.Net.Tool.AutoInject/AutoInjectExt.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 
 namespace Collapsenav.Net.Tool.AutoInject;
+
 public static class AutoInjectExt
 {
     /// <summary>
@@ -15,18 +16,21 @@ public static class AutoInjectExt
 
     public static IServiceCollection AddAutoInject(this IServiceCollection services)
     {
-        // 获取 CustomerTypes , 但是剔除 Collapsenav.Net.Tool 的 types
+        // 首先获取 CustomerTypes , 但是剔除 Collapsenav.Net.Tool 的 types
         var types = AppDomain.CurrentDomain.GetCustomerTypes().Where(item => !item.FullName!.StartsWith("Collapsenav.Net.Tool"));
         // 获取 service 中已经注册的 type
         var registedTypes = services.Select(item => item.ServiceType).ToList();
+        // 获取存在 AutoInject 标记属性的Types，以待后续进行注册
+        var autoInjectTypesTuple = types.Select(item => (ServiceType: item, InjectTypes: item.AttrMemberTypes<AutoInjectAttribute>()))
+            .Where(item=>item.InjectTypes.NotEmpty());
         // 获取被标记了 AutoInject 的 types
-        var autoInjectTypes = types.SelectMany(item => item.AttrMemberTypes<AutoInjectAttribute>())
+        var autoInjectTypes = autoInjectTypesTuple.SelectMany(item => item.InjectTypes)
         // 移除已经手动注册过的 type
-        .Where(item => !registedTypes.Contains(item)).Unique();
+            .Where(item => !registedTypes.Contains(item)).Unique();
         // 挑出需要注册的 接口
         var autoInjectInterfaces = autoInjectTypes.Where(item => item.IsInterface).ToList();
         // 挑出需要注册的 非接口
-        autoInjectTypes = autoInjectTypes.Where(item => !item.IsInterface).ToList();
+        autoInjectTypes = autoInjectTypes.Where(item => !item.IsInterface).Concat(autoInjectTypesTuple.Select(item=>item.ServiceType)).ToList();
         // 挑出所有 "实现"
         types = types.Where(item => !item.IsInterface && !item.IsAbstract);
 
